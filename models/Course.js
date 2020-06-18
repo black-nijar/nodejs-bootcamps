1ab5246a3b07a0561b5aba44c86a8a3a48bf9cfc
Nijarudeen <nijarr2020@gmail.com>
2020-06-18 21:26:12 +0530

    cal avgCost of single bootcamp

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,37 @@ const courseSchema = new mongoose.Schema({
   }
 });
 
+// Static methofd to get any of course tuition
+courseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log(`Calculating avg cost...`);
+
+  const obj = await this.aggregate([
+    {
+      $match : { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' }
+      }
+    }
+  ])
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10 ) * 10
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+// Call getAverage after save
+courseSchema.post('save', function() {
+  this.constructor.getAverageCost(this.bootcamp)
+});
+
+// Call getAverage Before remove
+courseSchema.pre('remove', function() {
+  this.constructor.getAverageCost(this.bootcamp)
+})
 module.exports = mongoose.model('Course', courseSchema);
